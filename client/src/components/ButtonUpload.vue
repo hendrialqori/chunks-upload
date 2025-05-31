36e6c26d6007c9839c6020b3d481d474c77984e4
Hendri Alqori <teamhendri18@gmail.com>
2025-05-31 14:12:40 +0700

    feat(client): create ButtonUpload component

---
+++ client/src/components/ButtonUpload.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <button v-bind="$attrs" class="text-xs bg-white text-black rounded-md px-3 h-6 cursor-pointer hover:outline-2">
+        <slot></slot>
+    </button>
+</template>
