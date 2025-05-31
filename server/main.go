052cf8af6555cd76bae7aea2693e12e49a47c853
Hendri Alqori <teamhendri18@gmail.com>
2025-05-31 14:09:46 +0700

    feat(apii): create server entry point and setup api route

--- server/main.go
+++ server/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"chunks-upload-server/handler"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/julienschmidt/httprouter"
+	"github.com/rs/cors"
+)
+
+func main() {
+	var (
+		route = httprouter.New()
+	)
+
+	if err := os.MkdirAll("./uploads", os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	route.POST("/upload", handler.UploadFile)
+	route.POST("/merge-chunks", handler.MergeChunks)
+
+	route.PanicHandler = func(w http.ResponseWriter, r *http.Request, i any) {
+		fmt.Println(i)
+
+		apiResponse := map[string]any{
+			"status":  http.StatusInternalServerError,
+			"message": i,
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(apiResponse); err != nil {
+			panic(err)
+		}
+	}
+
+	handler := cors.Default().Handler(route)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: handler,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
