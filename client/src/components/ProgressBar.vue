4e9b65b53dde4a8f8237206ab92596294c9411b4
Hendri Alqori <teamhendri18@gmail.com>
2025-05-31 14:13:03 +0700

    feat(client): create ProgressBar component

---
+++ client/src/components/ProgressBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+    progress: Number
+})
+</script>
+
+<template>
+    <div class="flex items-center gap-3" aria-label="progress">
+        <div class="w-full bg-white rounded-lg h-1">
+            <div :style="`width: ${props.progress}%`" class="h-full bg-blue-500 rounded-md"></div>
+        </div>
+        <span class="text-xs font-medium text-gray-600">{{ props.progress }}%</span>
+    </div>
+</template>
